9558f39822a6dff64ebd03f5829dc8496b63f47e
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2026-09-16 15:59:30 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'schedule-v5';
+const CACHE_NAME = 'schedule-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
