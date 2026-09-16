13a8cd0266a306164f99809c59770fb9c8ffe5b2
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2026-09-16 15:52:01 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'moy-den-v4';
+const CACHE_NAME = 'schedule-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
