0cebcd376b5a423ca1e21b84bf4940a599db4477
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2026-09-18 12:54:58 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'schedule-v12';
+const CACHE_NAME = 'schedule-v14';
 const ASSETS = [
   './', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './apple-splash-1179-2556.png', './apple-splash-1206-2622.png',
